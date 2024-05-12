251012b18067f714ef3a4149e51395ac3d7a968a
seeyoungryu <seeyoungryu@gmail.com>
2024-05-12 19:02:07 +0900

    add: entity

--- src/main/java/MemoEntity.java
+++ src/main/java/MemoEntity.java
@@ -1,2 +1,12 @@
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class MemoEntity {
+    private Long id;
+    private String username;
+    private String contents;
 }
